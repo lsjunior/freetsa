b8b39bab6f50ffb90dd76f0380f44e9be4b3917b
Lourival Sabino <lourival.junior@tse.jus.br>
2012-12-26 17:37:40 -0200

    mudanca para o framework 2.0.8

--- freetsa-web/src/test/java/br/net/woodstock/freetsa/web/test/CreateServerCertificateTest.java
+++ freetsa-web/src/test/java/br/net/woodstock/freetsa/web/test/CreateServerCertificateTest.java
@@ -5,16 +5,15 @@ import java.io.FileOutputStream;
 
 import junit.framework.TestCase;
 import br.net.woodstock.rockframework.security.Alias;
+import br.net.woodstock.rockframework.security.Identity;
 import br.net.woodstock.rockframework.security.cert.CertificateRequest;
 import br.net.woodstock.rockframework.security.cert.ExtendedKeyUsageType;
 import br.net.woodstock.rockframework.security.cert.KeyUsageType;
-import br.net.woodstock.rockframework.security.cert.PrivateKeyHolder;
-import br.net.woodstock.rockframework.security.cert.impl.BouncyCastleCertificateBuilder;
+import br.net.woodstock.rockframework.security.cert.impl.BouncyCastleCertificateGenerator;
 import br.net.woodstock.rockframework.security.store.KeyStoreType;
 import br.net.woodstock.rockframework.security.store.PasswordAlias;
 import br.net.woodstock.rockframework.security.store.PrivateKeyEntry;
 import br.net.woodstock.rockframework.security.store.Store;
-import br.net.woodstock.rockframework.security.store.StoreEntryType;
 import br.net.woodstock.rockframework.security.store.impl.JCAStore;
 
 public class CreateServerCertificateTest extends TestCase {
@@ -25,29 +24,31 @@ public class CreateServerCertificateTest extends TestCase {
 
 	public void testCreateSelfSignedCert() throws Exception {
 		CertificateRequest request = new CertificateRequest("Lourival Sabino");
-		request.withEmail("junior@woodstock.net.br");
-		request.withIssuer("Woodstock Tecnologia");
-		request.withKeyUsage(KeyUsageType.DIGITAL_SIGNATURE, KeyUsageType.NON_REPUDIATION, KeyUsageType.KEY_AGREEMENT);
-		request.withExtendedKeyUsage(ExtendedKeyUsageType.TIMESTAMPING);
-		PrivateKeyHolder holder = BouncyCastleCertificateBuilder.getInstance().build(request);
+		request.setEmail("junior@woodstock.net.br");
+		request.setIssuerName("Woodstock Tecnologia");
+		request.getKeyUsage().add(KeyUsageType.DIGITAL_SIGNATURE);
+		request.getKeyUsage().add(KeyUsageType.NON_REPUDIATION);
+		request.getKeyUsage().add(KeyUsageType.KEY_AGREEMENT);
+		request.getExtendedKeyUsage().add(ExtendedKeyUsageType.TIMESTAMPING);
+		Identity identity = BouncyCastleCertificateGenerator.getInstance().generate(request).getIdentity();
 
 		Store store = new JCAStore(KeyStoreType.PKCS12);
 		Alias alias = new PasswordAlias("tsaserver", "tsaserver");
-		PrivateKeyEntry privateKeyEntry = new PrivateKeyEntry(alias, holder.getPrivateKey(), holder.getChain());
+		PrivateKeyEntry privateKeyEntry = new PrivateKeyEntry(alias, identity);
 		store.add(privateKeyEntry);
 		store.write(new FileOutputStream("/tmp/tsaserver.pfx"), "tsaserver");
 	}
 
 	public void xtestCreateCA() throws Exception {
 		CertificateRequest request = new CertificateRequest("Woodstock Tecnologia CA");
-		request.withCa(true);
-		request.withComment("Woodstock Tecnologia CA");
-		request.withEmail("ca@woodstock.net.br");
+		request.setCa(true);
+		request.setComment("Woodstock Tecnologia CA");
+		request.setEmail("ca@woodstock.net.br");
 
-		PrivateKeyHolder holder = BouncyCastleCertificateBuilder.getInstance().build(request);
+		Identity identity = BouncyCastleCertificateGenerator.getInstance().generate(request).getIdentity();
 
 		Store store = new JCAStore(KeyStoreType.PKCS12);
-		store.add(new PrivateKeyEntry(new PasswordAlias("woodstock", "woodstock"), holder.getPrivateKey(), holder.getChain()));
+		store.add(new PrivateKeyEntry(new PasswordAlias("woodstock", "woodstock"), identity));
 		store.write(new FileOutputStream("/tmp/woodstock.pfx"), "woodstock");
 	}
 
@@ -55,19 +56,21 @@ public class CreateServerCertificateTest extends TestCase {
 		FileInputStream inputStream = new FileInputStream("/tmp/woodstock.pfx");
 		Store caStore = new JCAStore(KeyStoreType.PKCS12);
 		caStore.read(inputStream, "woodstock");
-		PrivateKeyEntry entry = (PrivateKeyEntry) caStore.get(new PasswordAlias("woodstock", "woodstock"), StoreEntryType.PRIVATE_KEY);
+		PrivateKeyEntry entry = (PrivateKeyEntry) caStore.get(new PasswordAlias("woodstock", "woodstock"));
 
 		CertificateRequest request = new CertificateRequest("Lourival Sabino");
-		request.withEmail("junior@woodstock.net.br");
-		request.withKeyUsage(KeyUsageType.DIGITAL_SIGNATURE, KeyUsageType.NON_REPUDIATION, KeyUsageType.KEY_AGREEMENT);
-		request.withExtendedKeyUsage(ExtendedKeyUsageType.TIMESTAMPING);
-		request.withIssuerKeyHolder(new PrivateKeyHolder(entry.getValue(), entry.getChain()));
+		request.setEmail("junior@woodstock.net.br");
+		request.getKeyUsage().add(KeyUsageType.DIGITAL_SIGNATURE);
+		request.getKeyUsage().add(KeyUsageType.NON_REPUDIATION);
+		request.getKeyUsage().add(KeyUsageType.KEY_AGREEMENT);
+		request.getExtendedKeyUsage().add(ExtendedKeyUsageType.TIMESTAMPING);
+		request.setIssuerIdentity(entry.toIdentity());
 
-		PrivateKeyHolder holder = BouncyCastleCertificateBuilder.getInstance().build(request);
+		Identity identity = BouncyCastleCertificateGenerator.getInstance().generate(request).getIdentity();
 
 		Store store = new JCAStore(KeyStoreType.PKCS12);
 		Alias alias = new PasswordAlias("tsaserver", "tsaserver");
-		PrivateKeyEntry privateKeyEntry = new PrivateKeyEntry(alias, holder.getPrivateKey(), holder.getChain());
+		PrivateKeyEntry privateKeyEntry = new PrivateKeyEntry(alias, identity);
 		store.add(privateKeyEntry);
 		store.write(new FileOutputStream("/tmp/tsaserver.pfx"), "tsaserver");
 	}
